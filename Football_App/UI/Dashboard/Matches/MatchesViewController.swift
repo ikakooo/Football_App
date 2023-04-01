da26dcc5b4029df140ddcd63477f92d95a5e2266
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-04-01 19:27:32 +0400

    project menaging up

--- Football_App/UI/Dashboard/Matches/MatchesViewController.swift
+++ Football_App/UI/Dashboard/Matches/MatchesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MatchesViewController.swift
 //  Football_App
 //
 //  Created by Irakli Chkhitunidze on 4/1/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MatchesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
