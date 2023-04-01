0ceb38719f19d86a2c7827399479e3d8da32b652
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2023-04-01 19:11:32 +0400

    init project

---
+++ Football_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football_App
+//
+//  Created by Irakli Chkhitunidze on 4/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
